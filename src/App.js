b5776b9f2645bd883469f2c955d319b31bded2f3
cph-mtnl <mtnl@cphbusiness.dk>
2022-03-07 12:39:48 +0100

    added example for useContext

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Header from "./components/Header";
 import data from "./data/data";
 import "./app.css";
 import TravelList from "./components/TravelList";
+import MyGlobalContext from "./context/my-context";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <TravelList data={data} />
+      <MyGlobalContext.Provider value={data}>
+        <TravelList />
+      </MyGlobalContext.Provider>
     </div>
   );
 }
--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,9 +1,12 @@
+import { useContext } from "react";
+import MyGlobalContext from "../context/my-context";
 import TravelEntity from "./TravelEntity";
 
-const TravelList = ({data}) => {
+const TravelList = () => {
+    const context = useContext(MyGlobalContext);
     return (
         <div className="travel-list">
-            {data.map((entity) => {
+            {context.map((entity) => {
                 return <TravelEntity 
                             key={entity.key} 
                             entity={entity} 
